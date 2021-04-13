def7cd0e5a581e1ddfa58711b431e26e71f920c5
RIKO-tlov <bluebyrair@outlook.jp>
2021-04-13 11:52:06 +0000

    [add]rspec_test

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'ef52896641443db5be6c7265149ad803efb0cdd918a8562520ce83326ded178edf806c91166441344522a7a285bb2d7c4b4e8365c7bdd6d38bbd09f9542e10ca'
